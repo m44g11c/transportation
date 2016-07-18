10d86dc904e67ff2ad517208281c215c144b8173
oOLokiOo <ivan.volkov.older@gmail.com>
2016-07-18 22:12:54 +0300

    change project directory structure

---
+++ app/views/index_view.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Moving Company</title>
+
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div id="topContainer">
+
+<!-- HEADER -->
+
+<div id="header">
+
+<div id="logo"><a href="#" title="Moving Company" ><img src="images/logo.gif" alt="Moving Company" width="227" height="46" border="0" /></a></div>
+
+<ul>
+<li class="home"><a href="#" title="Home Page">Home</a></li>
+<li class="bullet"></li>
+<li class="about"><a href="#" title="About">About</a></li>
+<li class="bullet"></li>
+<li class="services"><a href="#" title="Services">Services</a></li>
+<li class="bullet"></li>
+<li class="prices"><a href="#" title="Prices">Prices</a></li>
+<li class="bullet"></li>
+<li class="support"><a href="#" title="Support">Support</a></li>
+<li class="bullet"></li>
+<li class="contacts"><a href="#" title="Contacts">Contacts</a></li>
+</ul>
+</div>
+
+<!-- MAIN IMG -->
+
+<div id="mainimg">
+
+<div id="presentation"><a href="#"><img src="images/btn_presentation.gif" alt="View presentation" width="238" height="15" border="0" /></a></div>
+<div id="call"><img src="images/call.gif" alt="" width="213" height="70" /></div>
+</div>
+
+<!-- CONTENT -->
+
+<div id="content">
+
+<div id="leftPan">
+
+<div id="news">
+<h2></h2>
+
+<ul>
+<li><p><span>20.01.08 |</span> consetetur sadi lorem sed  et ea consetetur sadi pscing pscing elitr, sed lorem diam <a href="#">| more</a></p></li>
+<li><p><span>19.01.08 |</span> consetetur sadi lorem sed  et ea consetetur sadi pscing pscing elitr, sed diam <a href="#">| more</a></p></li>
+</ul>
+</div>
+
+<div id="testimonials">
+<h2></h2>
+<h3></h3>
+
+<p class="bluetxt">Dolores et ea lorem sadi pscing elit </p>
+
+<p>consetetur sadi lorem sed  et ea consetetur sadi consetetur sadi lorem sed  et ea </p>
+
+<p class="yellowtxt">Anne Michaels<br /><span>New York</span></p>
+</div>
+</div>
+
+<div id="rightPan">
+
+<div id="welcome">
+<h2></h2>
+<img src="images/img_welcome.jpg" alt="" width="120" height="88" />
+
+<p class="bluetxt">This is a free XHTML/CSS-based website template designed by <a href="http://www.templatesland.com">TemplatesLand.com</a></p>
+
+<p>consetetur sadi lorem sed  et ea consetetur sadi consetetur sadi lorem sed  et ea consetetur sadi pscing  consetetur consetetur sadi lorem sed  et ea consetetur sadi pscing consetetur pscing elitr, sed diam <a href="#">| more</a></p>
+</div>
+
+<div id="services">
+<h2></h2>
+
+<ul>
+<li><a href="#">Ascing elitDolores et ea consetetur</a></li>
+<li><a href="#">consetetur sadi lorem sed  et ea </a></li>
+<li><a href="#">consetetur sadi pscing consetetur </a></li>
+<li><a href="#">pscing elitr, sed diam consetetur sadi </a></li>
+<li><a href="#">lorem sed  et ea consetetur sadi pscing </a></li>
+<li><a href="#">elit Dolores et ea consetetur consetetur </a></li>
+<li><a href="#">sadi lorem sed  et ea consetetur sadi</a></li>
+</ul>
+
+<ul>
+<li><a href="#">Ascing elitDolores et ea consetetur</a></li>
+<li><a href="#">consetetur sadi lorem sed  et ea </a></li>
+<li><a href="#">consetetur sadi pscing consetetur </a></li>
+<li><a href="#">pscing elitr, sed diam consetetur sadi </a></li>
+<li><a href="#">lorem sed  et ea consetetur sadi pscing </a></li>
+<li><a href="#">elit Dolores et ea consetetur consetetur </a></li>
+<li><a href="#">sadi lorem sed  et ea consetetur sadi</a></li>
+</ul>
+</div>
+</div>
+<div class="clear"></div>
+</div>
+</div>
+
+<div id="bottomContainer">
+
+<!-- FOOTER -->
+
+<div id="footer">
+<p><a href="#">HOME</a> | <a href="#">ABOUT US</a> | <a href="#">SERVICES</a> | <a href="#">PRICES</a> | <a href="#">NEWS</a> | <a href="#">SUPPORT</a> | <a href="#">CONTACTS</a><br/>
+Copyright &copy; Your Company Name. Designed by<a href="http://www.templatesland.com"> TemplatesLand.com</a>.</p>
+</div>
+
+
+</div>
+<div class="sponsor">Sponsored by<a href="http://www.wix.com/free/website?utm_campaign=templateyes&amp;experiment_id=templateyesexp" title="Free Flash Templates" target="_blank"><img src="images/banner-728x90.jpg" alt="Free Flash Templates" width="728" height="90" border="0" /></a></div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__.'/app/views/index_view.php';
