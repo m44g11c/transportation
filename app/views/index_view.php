6d716ab6a573200cdfd55e76e27c48d3243552f7
m44g11c <abtoputet@tut.by>
2016-07-19 13:24:43 +0300

    add bootstrap

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -1,123 +1,77 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Moving Company</title>
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
 
-<link href="/public/css/styles.css" rel="stylesheet" type="text/css" />
+<title>Starter Template for Bootstrap</title>
 
-</head>
+<!-- Bootstrap core CSS -->
+<link href="/public/css/bootstrap.min.css" rel="stylesheet">
 
-<body>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-<div id="topContainer">
+<!-- Custom styles for this template -->
+<link href="/public/css/styles.css" rel="stylesheet">
 
-<!-- HEADER -->
+<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-<div id="header">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
 
-<div id="logo"><a href="#" title="Moving Company" ><img src="/public/images/logo.gif" alt="Moving Company" width="227" height="46" border="0" /></a></div>
+    <body>
 
-<ul>
-<li class="home"><a href="#" title="Home Page">Home</a></li>
-<li class="bullet"></li>
-<li class="about"><a href="#" title="About">About</a></li>
-<li class="bullet"></li>
-<li class="services"><a href="#" title="Services">Services</a></li>
-<li class="bullet"></li>
-<li class="prices"><a href="#" title="Prices">Prices</a></li>
-<li class="bullet"></li>
-<li class="support"><a href="#" title="Support">Support</a></li>
-<li class="bullet"></li>
-<li class="contacts"><a href="#" title="Contacts">Contacts</a></li>
-</ul>
-</div>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
 
-<!-- MAIN IMG -->
+      <div class="container">
 
-<div id="mainimg">
+        <div class="starter-template">
+          <h1>Bootstrap starter template</h1>
+          <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        </div>
 
-<div id="presentation"><a href="#"><img src="/public/images/btn_presentation.gif" alt="View presentation" width="238" height="15" border="0" /></a></div>
-<div id="call"><img src="/public/images/call.gif" alt="" width="213" height="70" /></div>
-</div>
-
-<!-- CONTENT -->
-
-<div id="content">
-
-<div id="leftPan">
-
-<div id="news">
-<h2></h2>
-
-<ul>
-<li><p><span>20.01.08 |</span> consetetur sadi lorem sed  et ea consetetur sadi pscing pscing elitr, sed lorem diam <a href="#">| more</a></p></li>
-<li><p><span>19.01.08 |</span> consetetur sadi lorem sed  et ea consetetur sadi pscing pscing elitr, sed diam <a href="#">| more</a></p></li>
-</ul>
-</div>
-
-<div id="testimonials">
-<h2></h2>
-<h3></h3>
-
-<p class="bluetxt">Dolores et ea lorem sadi pscing elit </p>
-
-<p>consetetur sadi lorem sed  et ea consetetur sadi consetetur sadi lorem sed  et ea </p>
-
-<p class="yellowtxt">Anne Michaels<br /><span>New York</span></p>
-</div>
-</div>
-
-<div id="rightPan">
-
-<div id="welcome">
-<h2></h2>
-<img src="/public/images/img_welcome.jpg" alt="" width="120" height="88" />
-
-<p class="bluetxt">This is a free XHTML/CSS-based website template designed by <a href="http://www.templatesland.com">TemplatesLand.com</a></p>
-
-<p>consetetur sadi lorem sed  et ea consetetur sadi consetetur sadi lorem sed  et ea consetetur sadi pscing  consetetur consetetur sadi lorem sed  et ea consetetur sadi pscing consetetur pscing elitr, sed diam <a href="#">| more</a></p>
-</div>
-
-<div id="services">
-<h2></h2>
-
-<ul>
-<li><a href="#">Ascing elitDolores et ea consetetur</a></li>
-<li><a href="#">consetetur sadi lorem sed  et ea </a></li>
-<li><a href="#">consetetur sadi pscing consetetur </a></li>
-<li><a href="#">pscing elitr, sed diam consetetur sadi </a></li>
-<li><a href="#">lorem sed  et ea consetetur sadi pscing </a></li>
-<li><a href="#">elit Dolores et ea consetetur consetetur </a></li>
-<li><a href="#">sadi lorem sed  et ea consetetur sadi</a></li>
-</ul>
-
-<ul>
-<li><a href="#">Ascing elitDolores et ea consetetur</a></li>
-<li><a href="#">consetetur sadi lorem sed  et ea </a></li>
-<li><a href="#">consetetur sadi pscing consetetur </a></li>
-<li><a href="#">pscing elitr, sed diam consetetur sadi </a></li>
-<li><a href="#">lorem sed  et ea consetetur sadi pscing </a></li>
-<li><a href="#">elit Dolores et ea consetetur consetetur </a></li>
-<li><a href="#">sadi lorem sed  et ea consetetur sadi</a></li>
-</ul>
-</div>
-</div>
-<div class="clear"></div>
-</div>
-</div>
-
-<div id="bottomContainer">
-
-<!-- FOOTER -->
-
-<div id="footer">
-<p><a href="#">HOME</a> | <a href="#">ABOUT US</a> | <a href="#">SERVICES</a> | <a href="#">PRICES</a> | <a href="#">NEWS</a> | <a href="#">SUPPORT</a> | <a href="#">CONTACTS</a><br/>
-Copyright &copy; Your Company Name. Designed by<a href="http://www.templatesland.com"> TemplatesLand.com</a>.</p>
-</div>
+      </div><!-- /.container -->
 
 
-</div>
-<div class="sponsor">Sponsored by<a href="http://www.wix.com/free/website?utm_campaign=templateyes&amp;experiment_id=templateyesexp" title="Free Flash Templates" target="_blank"><img src="/public/images/banner-728x90.jpg" alt="Free Flash Templates" width="728" height="90" border="0" /></a></div>
-</body>
-</html>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    
+
+  </body>
+  </html>
