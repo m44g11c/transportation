2c7e5daf4eb891fbaad3162ea430d1143368e409
oOLokiOo <ivan.volkov.older@gmail.com>
2016-07-19 14:34:33 +0300

    fix ie-emulation-modes-warning.js template path

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -21,8 +21,8 @@
 <link href="/public/css/styles.css" rel="stylesheet">
 
 <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-<script src="/assets/js/ie-emulation-modes-warning.js"></script>
+<!--[if lt IE 9]><script src="/public/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="/public/js/ie-emulation-modes-warning.js"></script>
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
 mb_internal_encoding('UTF-8');
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
-require_once __DIR__.'/app/views/index_view.php';
+
+define('ROOT', __DIR__);
+require_once ROOT.'/app/bootstrap.php';
