1b9554b0aedd74bd352bcce89b135b10203b6965
oOLokiOo <ivan.volkov.older@gmail.com>
2016-07-19 15:25:51 +0300

    update common views

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -1,66 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<meta name="description" content="">
-<meta name="author" content="">
-<link rel="icon" href="../../favicon.ico">
-
-<title>Starter Template for Bootstrap</title>
-
-<!-- Bootstrap core CSS -->
-<link href="/public/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<link href="/public/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-<!-- Custom styles for this template -->
-<link href="/public/css/styles.css" rel="stylesheet">
-
-<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-<!--[if lt IE 9]><script src="/public/js/ie8-responsive-file-warning.js"></script><![endif]-->
-<script src="/public/js/ie-emulation-modes-warning.js"></script>
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-    </head>
-
-    <body>
-
-      <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
-          </div>
-          <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-
-
-      <div class="container">
-
         <div class="row">
           <div id="main" class="col-sm-12  col-md-12  col-lg-12">
-            
           </div>
         </div>
 
@@ -77,6 +16,7 @@
               21.11.2012, 11:01</p>
           </div>
         </div>
+        
         <h1 class="text-center">УСЛУГИ</h1>
         <div class="row">
           <div class="col-md-4 service">
@@ -93,25 +33,9 @@
           </div>
         </div>
 
-      <div id="footer" class="row">
-        <div class="text-center">
-          <div><a href="../../../about">О нас</a> &nbsp;•&nbsp; <a href="../../../contacts">Контакты</a> &nbsp;&nbsp;&nbsp;© 2016 «deleloped by M4g1c and Loki» - Все права защищены.
+        <div id="footer" class="row">
+          <div class="text-center">
+            <div><a href="/about">О нас</a> &nbsp;•&nbsp; <a href="/contacts">Контакты</a> &nbsp;&nbsp;&nbsp;© 2016 «deleloped by M4g1c and Loki» - Все права защищены.
+            </div>
           </div>
         </div>
-      </div>
-
-      </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script> -->
-    <script src="/public/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/public/js/ie10-viewport-bug-workaround.js"></script>
-    
-
-  </body>
-  </html>
